6eac3895e908b4ce687c7408d93f2cf9fa67e2e7
danrlei-oliveira <danrlei.jesus@hotmail.com>
2023-10-12 18:18:22 -0300

    script em C sharp para calcular desconto

---
+++ BlackFriday/BlackFriday/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Black
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual o valor do produto: ");
+            float valor = float.Parse(Console.ReadLine());
+
+            float desconto = CalcDesconto(valor);
+            float valorAtual = CalcValorTotal(valor, desconto);
+
+            Console.WriteLine("Produto custava: {0:N2}", valor / 100);
+            Console.WriteLine("Com o desconto: {0:N2}", valorAtual / 100);
+            Console.WriteLine("Valor do desconto: {0:N2}", desconto / 100);
+        }
+
+        public static float CalcDesconto(float valor)
+        {
+            float desconto = (float)(valor * 0.6);
+            return desconto;
+        }
+
+        public static float CalcValorTotal(float valor, float desconto)
+        {
+            float valorAtual = valor - desconto;
+            return valorAtual;
+        }
+    }
+}
